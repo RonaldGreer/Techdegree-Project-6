b2c305268b20d1fc5701e94887e0a6e8edca6cc2
RonaldGreer <77591985+RonaldGreer@users.noreply.github.com>
2021-07-25 21:49:13 -0400

    issues

--- app.js
+++ app.js
@@ -18,4 +18,40 @@ a.addEventListener('click', () => {
 
 //gets a random phrase from my 'phrases' array
 const getRandomPhraseAsArray = arr => {
+    //the idea for using this random number generator function was suggested to me on slack by @Phillip
+    function random_num() {
+        const randomNum = phrases.length( Math.random() * 6 ) + 1;
+        return randomNum;
+    }
+    
+    console.log( phrases[randomNum] );
+    return getRandomPhrasesAsArray;
 }
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+    let items = ' ';
+    for ( let i = 0; i < phrases.length; i++) {
+        items += '<li>${ arr[i] }</li>';
+        
+    }
+    return items;
+}
+
+    document.getElementById('phrase').innerHTML = '<ul> ${addPhraseToDisplay(phrases)} </ul>';
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+});
