a6ffe3cb2e1c3a8765a162a9c1b8f2bc270e5298
RonaldGreer <77591985+RonaldGreer@users.noreply.github.com>
2021-07-31 14:43:18 -0400

    updates

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const missed = 0;
 const startDiv = document.querySelector('.start');
 
 const phrases = [
-    ['M', 'A', 'Y', ' ', 'T', 'H', 'E', ' ', 'F', 'O', 'R', 'C', 'E', ' ', 'B', 'E', ' ', 'W', 'I', 'T', 'H', ' ', 'Y', 'O', 'U'],
-    ['T', 'H', 'I', 'S', ' ', 'I', 'S', ' ', 'T', 'H', 'E', ' ', 'W', 'A', 'Y'],
-    ['N', 'E', 'V', 'E', 'R', ' ', 'T', 'E', 'L', 'L', ' ', 'M', 'E', ' ', 'T', 'H', 'E', ' ', 'O', 'D', 'D', 'S'],
-    ['I', ' ', 'H', 'A', 'V', 'E', ' ', 'A', ' ', 'B', 'A', 'D', ' ', 'F', 'E', 'E', 'L', 'I', 'N', 'G', ' ', 'A', 'B', 'O', 'U', 'T', ' ', 'T', 'H', 'I', 'S'],
-    ['H', 'E', 'L', 'L', 'O', ' ', 'T', 'H', 'E', 'R', 'E']
+    "may the force be with you",
+    "this is the way",
+    "never tell me the odds",
+    "I have a bad feeling about this",
+    "hello there"
 ];
 
 a.addEventListener('click', () => {
@@ -19,13 +19,9 @@ a.addEventListener('click', () => {
 //gets a random phrase from my 'phrases' array
 const getRandomPhraseAsArray = arr => {
     //the idea for using this random number generator function was suggested to me on slack by @Phillip
-    function random_num() {
-        const randomNum = phrases.length( Math.random() * 6 ) + 1;
-        return randomNum;
-    }
-    
-    console.log( phrases[randomNum] );
-    return getRandomPhrasesAsArray;
+    const randomNum = phrases.length( Math.random() * 5 ) + 1;
+    phrases[randomNum].split(' ');
+    return randomNum;
 }
 
 // adds the letters of a string to the display
