dd3a030ed003e8c4e4f91264cfec39f15c227458
RonaldGreer <77591985+RonaldGreer@users.noreply.github.com>
2021-08-06 23:44:14 -0400

    fixes and additions

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const div = document.getElementById('qwerty');
 const phraseDiv = document.getElementById('phrase');
 const a = document.querySelector('.btn__reset');
-const missed = 0;
+let missed = 0;
 const startDiv = document.querySelector('.start');
+const keyboard = document.querySelector('#qwerty');
+const letters = document.querySelectorAll('.letter');
+const misses = document.querySelector('.misses');
 //@Sarah (she/her) showed me the correct syntax my arrays should be in for this challenge, as well as suggesting using the split() method on slack.
 const phrases = [
     "may the force be with you",
@@ -12,6 +15,7 @@ const phrases = [
     "hello there"
 ];
 
+// listens for the start game button to be pressed
 a.addEventListener('click', () => {
     startDiv.style.display = 'none';
 });
@@ -25,8 +29,9 @@ const getRandomPhraseAsArray = arr => {
     return phraseArray;
 };
 getRandomPhraseAsArray(phrases);
-
-
+//credit
+let li = document.querySelector('li');
+let ul = document.querySelector('ul');
 // adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
         let items = ''; 
@@ -34,22 +39,23 @@ const addPhraseToDisplay = arr => {
         items += `<li>${ arr[i] }</li>`
         li.textContent = arr.value;
 
-        if (li.textContent === ' ') {
-            li.className = 'space';
-        } else {
-                li.className = 'letter';
-        }
+
         ul.appendChild(li);
 
     }
 
+    if (li.textContent === ' ') {
+        li.className = 'space';
+    } else {
+            li.className = 'letter';
+    }
+
     return items;
 };
-
+let phraseArray = getRandomPhraseAsArray(phrases);
+//
 document.querySelector('ul').innerHTML = `
-<ul>
   ${addPhraseToDisplay(phraseArray)}
-</ul>
 `;
 
 addPhraseToDisplay(phraseArray);
@@ -57,16 +63,37 @@ addPhraseToDisplay(phraseArray);
 
 // check if a letter is in the phrase
 const checkLetter = button => {
+
+    //The following code is from and based off of the office hours session for the unit 6 project on slack(hosted by @Amber(she/her)
+    let matched = null;
+
+    for (i=0; i < letters.length; i++) {
+        if (button === letters[i].textContent.toLowerCase()) {
+            letters[i].classList.add('show');
+            matched = true;
+        }
+    }
+    return matched;
 }
+
+
+// listen for the onscreen keyboard to be clicked
+keyboard.addEventListener('click', e => {
+
+     //The following code is from and based off of the office hours session for the unit 6 project on slack(hosted by @Amber(she/her)
+    if (e.target.tagName === "BUTTON") {
+        e.target.className = 'chosen';
+        e.target.disabled = true;
+        const match = checkLetter(e.target.textContent.toLowerCase());
+        if (match === null) {
+          missed++;
+          misses.textContent = missed;
+        }
+      }
+});
+
+
 
 // check if the game has been won or lost
 const checkWin = () => {
 }
-
-// listen for the start game button to be pressed
-startButton.addEventListener('click', () => {
-});
-
-// listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', e => {
-});
